7426313f6c370d734560d785d5ccd5f642c68649
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2023-09-30 00:14:23 -0500

    fron jsx config dev

---
+++ Front/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import '../css/index.css';
+// import Nav from "../containers/Nav";
+// import Banner from "../containers/Banner";
+const Home = () => {
+  return (
+    <div className="home">
+        <p>hola</p>
+      {/* <Nav />
+      <Banner /> */}
+    </div>
+  );
+};
+
+export default Home;
